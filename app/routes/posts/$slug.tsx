1a9b7a69421b00799015075142259b841ceddbde
Martin Bavio <mbavio@gmail.com>
2022-01-06 00:59:11 -0300

    Add some test headers

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "remix";
+import { HeadersFunction, useLoaderData, json } from "remix";
 import type { LoaderFunction } from "remix";
 import { postBySlugQuery } from "~/lib/queries";
 import createSanityClient, { getImageUrlBuilder } from "~/lib/client";
@@ -6,17 +6,29 @@ import PostHeader from "~/components/PostHeader";
 import { Post } from "~/lib/types";
 import PostContent from "~/components/PostContent";
 
-export const loader: LoaderFunction = async ({
-  params,
-  context,
-}): Promise<Post> => {
+export const loader: LoaderFunction = async ({ params, context }) => {
   const client = createSanityClient({ projectId: context.SANITY_PROJECT_ID });
   const imgUrlBuilder = getImageUrlBuilder(client);
-  const post = await client.fetch(postBySlugQuery, { slug: params.slug });
-  return {
-    ...post,
-    coverImage: imgUrlBuilder.image(post.coverImage).width(1200).url(),
-  };
+  const post: Post = await client.fetch(postBySlugQuery, { slug: params.slug });
+  return json<Post>(
+    {
+      ...post,
+      coverImage: imgUrlBuilder.image(post.coverImage).width(1200).url(),
+    },
+    {
+      status: 200,
+      headers: {
+        "Cache-Control": "max-age=300, s-maxage=3600",
+      },
+    }
+  );
+};
+
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+  const headers = new Headers();
+  headers.set("Cache-Control", "max-age=300, s-maxage=3600");
+  console.log("loaderHeaders CC", loaderHeaders.get("Cache-Control"));
+  return headers;
 };
 
 export default function Post() {
